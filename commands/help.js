bb80456b7ade9a92ac2b8e333cde47d795c4eaad
Chris Hayes <chrishayesmu@gmail.com>
2015-04-22 03:25:06 -0700

    Add help command linking to GitHub

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+var PlugBotBase = require("PlugBotBase");
+
+var LOG = new PlugBotBase.Log("HelpCommand");
+
+function handler(event, globalObject) {
+    LOG.info("Help command was called by {}", event.username);
+    globalObject.bot.sendChat("EmancipatorBot 2.0's help can be found here: https://github.com/chrishayesmu/EmancipatorBot/wiki/Help");
+}
+
+module.exports = {
+    handler: handler,
+    triggers: [ "help", "info" ]
+}
