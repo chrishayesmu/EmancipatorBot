985fd36dd045287c540c23be71a385c2b1fd566d
Chris Hayes <chrishayesmu@gmail.com>
2015-04-24 01:50:03 -0700

    Add functionality to detect and optionally skip songs which have been mehhed

---
+++ event_listeners/AutoSkipMehhedSongs.js
@@ -0,0 +1,71 @@
+/**
+ * Tracks how many mehs have been placed against the current song.
+ * If it meets a configuration-defined threshold, the bot will print
+ * a message to the room and may (depending on configuration) force
+ * skip the song.
+ */
+
+var PlugBotBase = require("PlugBotBase");
+
+var LOG = new PlugBotBase.Log("AutoSkipMehhedSongs");
+
+var alreadyWarnedOnCurrentSong = false;
+
+function handleAdvanceEvent() {
+    alreadyWarnedOnCurrentSong = false;
+}
+
+function handleVoteEvent(event, globalObject) {
+    if (alreadyWarnedOnCurrentSong) {
+        return;
+    }
+
+    var forceSkipMehhedSongs = globalObject.config.Emancipator.AutoSkipMehhedSongs.forceSkipMehhedSongs;
+    var percentageToSkipAt = globalObject.config.Emancipator.AutoSkipMehhedSongs.percentageToSkipAt;
+    var preventSingleMehFromSkipping = globalObject.config.Emancipator.AutoSkipMehhedSongs.preventSingleMehFromSkipping;
+
+    var currentPlay = globalObject.roomState.playHistory[0];
+    var numberOfMehs = currentPlay.votes.mehs.length;
+    var percentageOfRoomMehhing = 100 * (numberOfMehs / globalObject.roomState.usersInRoom.length);
+
+    if (percentageOfRoomMehhing > percentageToSkipAt && (!preventSingleMehFromSkipping || numberOfMehs > 1)) {
+        LOG.info("Current song exceeded meh skip threshold of {}%", percentageToSkipAt);
+        globalObject.bot.sendChat("The current song has exceeded the meh skip threshold of {}%.", percentageToSkipAt);
+        alreadyWarnedOnCurrentSong = true;
+        skipIfAllowed(globalObject);
+    }
+}
+
+function init(globalObject) {
+    var percentageToSkipAt = globalObject.config.Emancipator.AutoSkipMehhedSongs.percentageToSkipAt;
+
+    if (percentageToSkipAt < 1 || percentageToSkipAt > 100) {
+        throw new Error("Configuration error: Emancipator.AutoSkipMehhedSongs.percentageToSkipAt not in range 1-100; actual value is " + percentageToSkipAt);
+    }
+}
+
+/**
+ * Skips the current DJ if configuration allows for it.
+ *
+ * @param {object} globalObject - The global object from the event
+ */
+function skipIfAllowed(globalObject) {
+    if (globalObject.config.Emancipator.AutoSkipMehhedSongs.forceSkipMehhedSongs) {
+        LOG.info("Attempting to force skip DJ");
+        globalObject.bot.forceSkip(function(succeeded) {
+            if (succeeded) {
+                LOG.info("Successfully skipped a duplicate song.");
+            }
+            else {
+                LOG.warn("Failed to skip duplicate song. The bot may not have sufficient permissions in the room.");
+            }
+        });
+    }
+    else {
+        LOG.info("Not configured to force skip on duplicate plays. Doing nothing.");
+    }
+}
+
+module.exports[PlugBotBase.Event.ADVANCE] = handleAdvanceEvent;
+module.exports[PlugBotBase.Event.VOTE] = handleVoteEvent;
+module.exports.init = init;
