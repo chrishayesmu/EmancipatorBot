fdfe4b5d1d04b1eccd5d3adf540624f0941c7d1c
Chris Hayes <chrishayesmu@gmail.com>
2015-05-23 12:18:54 -0700

    Shorten message displayed in AutoSkipBuggedSongs

--- event_listeners/AutoSkipBuggedSongs.js
+++ event_listeners/AutoSkipBuggedSongs.js
@@ -18,7 +18,7 @@ function handleAdvanceEvent(event, globalObject) {
         var currentSong = globalObject.roomState.playHistory[0].media;
 
         if (currentSong.contentID === mediaContentID) {
-            bot.sendChat("The current song appears to be bugged and should have already ended.");
+            bot.sendChat("The current song is bugged and should have ended.");
 
             if (globalObject.config.Emancipator.AutoSkipBuggedSongs.forceSkipBuggedSongs) {
                 bot.forceSkip();
