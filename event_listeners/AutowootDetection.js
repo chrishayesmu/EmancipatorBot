e44e1eff31161dd799b202eeb0f16b3e94d0a0ec
Chris Hayes <chrishayesmu@gmail.com>
2015-05-25 19:15:51 -0700

    Change autowoot detection to use localStartDate and be more configurable

--- event_listeners/AutowootDetection.js
+++ event_listeners/AutowootDetection.js
@@ -7,10 +7,11 @@ function handleVoteEvent(event, globalObject) {
         return;
     }
 
+    var autowootDurationInMilliseconds = globalObject.config.Emancipator.AutowootDetection.autowootDurationInMilliseconds;
     var currentPlay = globalObject.roomState.playHistory[0];
     var now = Date.now();
 
-    if (event.vote === 1 && now - currentPlay.startDate < 2000) { // TODO make configuration-driven
+    if (event.vote === 1 && now - currentPlay.localStartDate < autowootDurationInMilliseconds) {
         var autowooter = globalObject.roomState.findUserInRoomById(event.userID);
         LOG.info("Found autowooting by {} (userID = {})", autowooter.username, autowooter.userID);
         globalObject.bot.sendChat("@{} Auto-wooting is not allowed in this room. Please disable your autowoot script.", autowooter.username);
