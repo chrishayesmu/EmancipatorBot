88d1054f5c9a4e539fb2374138a8b58899ff2159
Chris Hayes <chrishayesmu@gmail.com>
2016-12-28 13:14:49 -0800

    Change meme command to accept a meme number instead of just being random

--- commands/meme.js
+++ commands/meme.js
@@ -6,9 +6,24 @@ function init(globalObject) {
 
 function handler(event, globalObject) {
     var bot = globalObject.bot;
-    var index = getRandomInt(0, memes.length);
+    var index;
 
-    bot.sendChat(memes[index]);
+    if (event.args && event.args.length > 0 && Number.isInteger(event.args[0])) {
+        index = Number(event.args[0]) - 1; // argument is provided as 1-indexed
+    }
+    else {
+        index = getRandomInt(0, memes.length);
+    }
+
+    var totalMemes = memes.length;
+    var memeNumber = index + 1;
+
+    if (index >= memes.length || index < 0) {
+        bot.sendChat("Meme {} doesn't exist; memes go from 1 to {}", memeNumber, totalMemes);
+    }
+    else {
+        bot.sendChat("Meme {} out of {} {}", memeNumber, totalMemes, memes[index]);
+    }
 }
 
 function getRandomInt(minInclusive, maxExclusive) {
